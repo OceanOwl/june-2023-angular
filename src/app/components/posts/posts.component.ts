3ae018222b84aca5a5ff9bac2e4158a0c694f26b
OceanOwl <115832268+OceanOwl@users.noreply.github.com>
2023-12-06 01:36:39 +0100

    first try

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -2,11 +2,14 @@ import {Component, Input} from '@angular/core';
 import {PostsService} from "../../services/posts.service";
 import {IPost} from "../../interfaces/post.interface";
 import {IUser} from "../../interfaces";
+import {PostComponent} from "../post/post.component";
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [],
+  imports: [
+    PostComponent
+  ],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.css'
 })

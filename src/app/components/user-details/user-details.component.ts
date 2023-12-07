1c6dfcdaff9c1a034ad2b6cc5e53a68afc61c98a
OceanOwl <115832268+OceanOwl@users.noreply.github.com>
2023-12-07 22:54:53 +0100

    first step

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -3,9 +3,6 @@ import {ActivatedRoute, Router, RouterLink} from "@angular/router";
 
 import {IPost, IUser} from "../../interfaces";
 import {PostsService, UserService} from "../../services";
-import {routes} from "../../app.routes";
-import {PostsPageComponent} from "../../pages";
-import {transition} from "@angular/animations";
 
 @Component({
   selector: 'app-user-details',
@@ -31,15 +28,15 @@ export class UserDetailsComponent implements OnInit {
       if (!this.user) {
         this.userService.getById(id).subscribe(value => this.user = value)
       }
-
     })
   }
 
   ngOnInit(): void {
+
   }
 
   getPosts(userId: number) {
-    this.postsService.getByUserId(this.user.id).subscribe(value => this.posts = value)
-    // console.log(this.posts);
+    this.postsService.getByUserId(userId).subscribe(value => this.posts = value)
+    console.log(this.posts);
   }
 }
